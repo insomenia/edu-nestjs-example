bf8c77c347d39e8a131139d5975433020480e3f3
insomenia-hongjun <hongjunjeong95@insomenia.com>
2021-11-02 16:28:25 +0900

    Add hasing with password

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,8 +1,35 @@
-import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import {
+  INestApplication,
+  Injectable,
+  InternalServerErrorException,
+  OnModuleInit,
+} from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super();
+    this.$use(async (params, next) => {
+      if (params.model === 'User' && params.action === 'create') {
+        if (params.args.data['password']) {
+          try {
+            params.args.data['password'] = await bcrypt.hash(
+              params.args.data['password'],
+              10,
+            );
+          } catch (e) {
+            console.error(e);
+            throw new InternalServerErrorException();
+          }
+        }
+      }
+      const result = await next(params);
+      return result;
+    });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
